1eb519d0a3e7f0334183f55cf9d7106c862e8b19
Jasmine Herd <jthm66@mail.missouri.edu>
2018-04-03 22:15:59 -0500

    User-Signup rough layout

---
+++ main.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = '''
+    <style>
+        .error {{ color: red; }}
+    </style>
+    <h1>User Signup</h1>
+    <form method='POST'>
+
+        <label>Username
+            <input name="username" type="text" value='{username}' />
+        </label>
+        <span class="error">{user_error}</span>
+        <br>
+
+        <label>Password
+            <input name="password" type="password" value='{password}' />
+        </label>
+        <span class="error">{pass_error}</span>
+        <br>
+        <label>Verify Password
+            <input name="passwordVerify" type="password" value='{passwordVerify}' />
+        </label>
+        <span class="error">{passVerify_error}</span>
+        <br>
+
+        <label>Email(optional)
+            <input name="email" type="text" value='{email}' />
+        </label>
+        <span class="error">{email_error}</span>
+        <input type="submit" value="submit" />
+
+        
+    </form>
+
+'''
+
+
+@app.route("/homepage")
+def index():
+    return form.format(username='',user_error='',
+    password='',pass_error ='',
+    passwordVerify = '', passVerify_error = '',
+    email = '',email_error = '')
+
+@app.route('/homepage',methods=['POST'])
+def homepage():
+    username = request.form['username']
+    password =request.form['password']
+    passwordVerify=request.form['passwordVerify']
+    email = request.form['email']
+
+    user_error = ''
+    pass_error = ''
+    passVerify_error = ''
+    email_error = ''
+
+
+    #USER ERROR
+    if len(username)==0:
+       user_error = "Error"
+
+    if len(password) == 0:
+        pass_error = "Enter Password"
+    
+    if len(passwordVerify)== 0:
+        passVerify_error = "Verify Password"
+    
+    if len(email) > 0 and len(email) < 3:
+        email_error = "enter valid email"
+
+
+    if not user_error and not pass_error and not passVerify_error and not email_error:
+        return redirect('/welcomePage')
+    else:
+        return form.format(username=username,user_error=user_error,
+        password = password,pass_error=pass_error,
+        passVerify_error=passVerify_error,passwordVerify=passwordVerify,
+        email=email, email_error=email_error)
+
+@app.route("/welcomePage/" )
+def welcomePage():
+    return ("NO ERRORS WOOT WOOT")
+
+app.run()
